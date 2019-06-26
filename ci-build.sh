585287e33a09a3a66ecd716a3db3cd92f8dad558
Rinat Zaripov <zaripov.rinat@gmail.com>
2019-06-26 14:17:21 +1000

    feat: snips base image

--- ci-build.sh
+++ ci-build.sh
@@ -1,20 +1,27 @@
 if [[ "$USE_CACHE_FROM" == "1" ]]; then
   CACHE_FROM_BUILD='--cache-from aimeeblue/ab-recorder-build-base:latest'
   CACHE_FROM_RUN='--cache-from aimeeblue/ab-recorder-run-base:latest'
+  CACHE_FROM_SNIPS='--cache-from aimeeblue/ab-recorder-run-base-snips:latest'
 else
   CACHE_FROM_BUILD=''
   CACHE_FROM_RUN=''
+  CACHE_FROM_SNIPS=''
 fi
 
 echo \# Settings: 
 echo \# USE_CACHE_FROM'   '= $USE_CACHE_FROM '(set to 1 to use --cache-from)'
 echo \# CACHE_FROM_BUILD' '= $CACHE_FROM_BUILD
 echo \# CACHE_FROM_RUN'   '= $CACHE_FROM_RUN
+echo \# CACHE_FROM_SNIPS' '= $CACHE_FROM_SNIPS
 
 BUILD_BASE="docker build $CACHE_FROM_BUILD --tag aimeeblue/ab-recorder-build-base:new -f 1.build-base.Dockerfile ."
 TAG_BASE="docker tag aimeeblue/ab-recorder-build-base:new aimeeblue/ab-recorder-build-base:latest"
+
 BUILD_RUN="docker build $CACHE_FROM_RUN --tag aimeeblue/ab-recorder-run-base:new -f 2.run-base.Dockerfile ."
 TAG_RUN="docker tag aimeeblue/ab-recorder-run-base:new aimeeblue/ab-recorder-run-base:latest"
+
+BUILD_SNIPS="docker build $CACHE_FROM_SNIPS --tag aimeeblue/ab-recorder-run-base-snips:new -f 3.run-base-snips.Dockerfile ."
+TAG_SNIPS="docker tag aimeeblue/ab-recorder-run-base-snips:new aimeeblue/ab-recorder-run-base-snips:latest"
 
 echo \# 🚀 $BUILD_BASE \
   && $BUILD_BASE \
--- ci-push.sh
+++ ci-push.sh
@@ -1,3 +1,4 @@
 docker login -u=$DOCKER_USERNAME -p=$DOCKER_PASSWORD \
     && docker push aimeeblue/ab-recorder-build-base \
-    && docker push aimeeblue/ab-recorder-run-base
+    && docker push aimeeblue/ab-recorder-run-base \
+    && docker push aimeeblue/ab-recorder-run-base-snips
